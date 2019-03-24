541679a504d1becb7e488ef69364f6f043d68949
serban_b <serban_b@sectips.net>
2019-03-25 00:21:38 +0200

    First commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+
+## Intro
+
+`Jalpc-docker-compose` is a fast and easy way to deploy Jalpc, a Jakyll theme as docker behind Nginx.
+
+#### Key features
+
+* Microservices
+* Nginx SSLlabs - 100%
+* Ease of installation
+* Easy to use.
+
+#### Installation
+
+You can follow the blog post: https://www.sectips.net/other/2019/03/24/Easy-blog-with-docker-compose-and-Jekyll.html
+
+## External/related projects
+
+#### Jekyll documentation
+* https://jekyllrb.com/docs/
+
+#### Jalpc - Jekyll Theme
+* https://github.com/jarrekk/Jalpc
---
+++ change_name.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#A script to enumerate local information from a Linux host
+
+#help function
+usage() 
+{ 
+echo -e "\n\e[00;31m#########################################################\e[00m" 
+echo -e "\e[00;31m#\e[00m" "\e[00;33mChange domain name\e[00m" "\e[00;31m#\e[00m"
+echo -e "\e[00;31m#########################################################\e[00m"
+echo -e "\e[00;33m# Example: ./LinEnum.sh example.com \e[00m"
+echo -e "\e[00;31m#########################################################\e[00m"		
+}
+
+if [ $(( $# - $OPTIND )) -lt 0 ]; then
+    usage
+    exit 1
+fi
+
+if [ ! -d "Jalpc" ]; then
+    echo "First clone Jalpc project: git clone https://github.com/jarrekk/Jalpc.git"
+    exit 1
+fi
+
+ARG=${@:$OPTIND:1}
+
+regex='.*\..*\..*'
+[[ ! $ARG =~ $regex ]] && (echo "domain name should be CNAME, eg www.example.com"; exit 1)
+
+sed -i -- "s|url: https.*|url: https://$ARG/|g" Jalpc/_config.yml
+sed -i -- "s|baseurl:.*|#baseurl: /|g" Jalpc/_config.yml
+sed -i -- "s/www.yourdomain.com/$ARG/g" nginx-lets-encrypt/nginx.conf
+sed -i -- "s/yourdomain.com/${ARG#*.}/g" nginx-lets-encrypt/nginx.conf
+echo $ARG > Jalpc/CNAME
